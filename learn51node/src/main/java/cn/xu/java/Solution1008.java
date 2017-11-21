735d0b371630f9cc917231ab75949b3aa0838b82
徐文学 <xuwenxue@corp.autohome.inc>
2017-11-21 10:32:07 +0800

    最大公共子序列

--- learn51node/src/main/java/cn/xu/java/Solution1008.java
+++ learn51node/src/main/java/cn/xu/java/Solution1008.java
@@ -3,6 +3,19 @@ package cn.xu.java;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
+/**
+ * 输入N和P（P为质数），求N! Mod P = ? (Mod 就是求模 %)
+ 例如：n = 10， P = 11，10! = 3628800
+ 3628800 % 11 = 10
+ Input
+ 两个数N,P，中间用空格隔开。(N < 10000, P < 10^9)
+ Output
+ 输出N! mod P的结果。
+ Input示例
+ 10 11
+ Output示例
+ 10
+ */
 public class Solution1008 {
    public static void main(String[] args) {
       Scanner in = new Scanner(System.in);
@@ -12,7 +25,7 @@ public class Solution1008 {
       int p = in.nextInt();
       long result = 1;
       for(int i=n;i>1;i--){
-         result = (result*i)/p;
+         result = (result*i)%p;
       }
       out.println(result);
       out.flush();
